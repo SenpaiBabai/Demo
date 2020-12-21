69f62bbd98832a1496d8d33e504f254a3ca3a919
SenpaiBabai <65128636+SenpaiBabai@users.noreply.github.com>
2020-12-21 12:04:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SiriusN16CXRseg", 
-    version="0.0.2",
+    name="SenpaiBabai", 
+    version="0.0.1",
     author="Sirius_nauka16",
     author_email="dkhasanov76@gmail.com",
-    description="Проект по сегментации легких.",
+    description="Проект по сегментации легких. TECT",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SenpaiBabai/CXRsegmentation",
     packages = find_packages(),
     package_data = {
-        'CXRsegmentation': [
-            'trained_models/Model_lungs_heart',
+        'testing': [
+            'Testing.py',
         ]
     },
     classifiers=[
